a065f0a3bb53b6c2352d0e9a00521ca28a19715a
codedubs <35834450+codedubs@users.noreply.github.com>
2019-05-03 00:48:06 -0700

    Checkpoint 3 routing, complete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 module.exports = app;
 
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init(app, express);
 routeConfig.init(app);
---
+++ src/config/main-config.js
@@ -0,0 +1,11 @@
+
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+
+module.exports ={
+  init(app, express) {
+    app.set("views", viewsFolder);
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, "..", "assets")));
+  }
+};
