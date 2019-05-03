b1cfd5998566774cffb297873ae77e3ebb34b069
codedubs <35834450+codedubs@users.noreply.github.com>
2019-05-03 02:02:05 -0700

    Checkpoint 3 routing, complete

--- src/routes/static.js
+++ src/routes/static.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const staticController = require("../controllers/staticController");
 
+
 router.get("/", staticController.index);
+
+
 
 module.exports = router;
