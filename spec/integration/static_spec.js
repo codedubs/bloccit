21a615879a369bcf961e307a06b9be5bf1a9c222
codedubs <35834450+codedubs@users.noreply.github.com>
2019-05-03 01:53:06 -0700

    Assignment 1 introexpress, redo

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -7,23 +7,22 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
 
-        done();
       });
     });
 
-    it("should contain the string polo", (done) => {
+    it("should return status code 200 and have 'polo' in the body of the response", () => {
       request.get("/marco", (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(res.body).toBe("polo");
+        expect(body).toContain("polo");
 
-        done();
       });
     });
 
   });
-  
+
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const staticController = require("../controllers/staticController");
+
 
 router.get("/", (req, res, next) => {
   res.send("Welcome to Bloccit");
 });
+
+router.get("/marco", (req, res, next) => {
+  res.send("polo")
+})
 
 module.exports = router;
