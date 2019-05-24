68b4dbb32e7612591367c0804eaee0b193a65042
codedubs <35834450+codedubs@users.noreply.github.com>
2019-05-24 03:17:36 -0700

    Checkpoint 5 topic resource part 2, complete

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,9 +1,10 @@
 const Topic = require("./models").Topic;
 
+
 module.exports = {
 
 
-  getAllTopics(callback){
+  getAllTopics(callback) {
     return Topic.all()
 
     .then((topics) => {
@@ -12,5 +13,42 @@ module.exports = {
     .catch((err) => {
       callback(err);
     })
+  },
+
+  getTopic(id, callback) {
+    return Topic.findById(id)
+
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  addTopic(newTopic, callback) {
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+
+  deleteTopic(id, callback) {
+    return Topic.destroy({
+      where: {id}
+    })
+    .then((topic) => {
+      callback(null, topic);
+    })
+    .catch((err) => {
+      callback(err);
+    })
   }
+
 }
