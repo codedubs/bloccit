afa9a17aec7084f22100a8e2291a49c3fe9ac9ba
codedubs <35834450+codedubs@users.noreply.github.com>
2019-06-25 04:14:59 -0700

    Checkpoint 9 authentication, complete

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,6 +1,7 @@
 
 
 module.exports = {
+
   validatePosts(req, res, next) {
     if(req.method === "POST") {
       req.checkParams("topicId", "must be valid").notEmpty().isInt();
@@ -13,6 +14,23 @@ module.exports = {
     if(errors) {
       req.flash("error", errors);
       return res.redirect(303, req.headers.referer)
+    } else {
+      return next();
+    }
+  },
+
+  validateUsers(req, res, next) {
+    if(req.method === "POST") {
+      req.checkBody("email", "must be valid").isEmail();
+      req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6});
+      req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
+    }
+
+    const errors = req.validationErrors();
+
+    if(errors) {
+      req.flash("error", errors);
+      return res.redirect(req.headers.referer);
     } else {
       return next();
     }
